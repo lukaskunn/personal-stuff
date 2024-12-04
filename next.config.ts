ced24545704093b6a67e3ffc316fa30c0c9a6ff5
Lucas Oliveira <lucassioliveira098@gmail.com>
2024-12-03 21:38:56 -0300

    update files to enable multizones

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/my-stuff",
-  assetPrefix: "/my-stuff-static/",
+  assetPrefix: "/my-stuff-static",
 };
 
 export default nextConfig;
+
