ba635f7ac7d17a04325617deaa475ee8767f60b4
Lucas Oliveira <lucassioliveira098@gmail.com>
2026-04-15 23:43:56 -0300

    fix: Correct typo in slug for CanvasGrid component

--- src/app/project/canvas-grid-cursor-follower/CanvasGridClient.tsx
+++ src/app/project/canvas-grid-cursor-follower/CanvasGridClient.tsx
@@ -58,7 +58,7 @@ export default function CanvasGridClient() {
           projectName={info.title}
           description={info.description}
           technologies={info.technologies}
-          slug="canvas-grid-cusor-follower"
+          slug="canvas-grid-cursor-follower"
           controls={controls}
         />
       </div>
